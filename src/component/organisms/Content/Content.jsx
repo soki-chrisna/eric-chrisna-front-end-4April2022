0489e9e56883ac4fc9f56ffc81ab1c39ab587877
MITRAIS\Eric_C <eric.chrisna@mitrais.com>
2022-04-09 23:56:52 +0800

    Added Main Content Wrapper to app skeleton

---
+++ src/component/organisms/Content/Content.jsx
@@ -0,0 +1,14 @@
+
+import Container from '@mui/material/Container';
+
+import Routes from '../../../routes';
+
+const Content = () => {
+  return (
+    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      <Routes />
+    </Container>
+  )
+}
+
+export default Content;
