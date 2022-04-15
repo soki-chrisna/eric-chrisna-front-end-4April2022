da003eba8b2ae5af925058159c72e0d9fb4fa4db
MITRAIS\Eric_C <eric.chrisna@mitrais.com>
2022-04-15 17:05:23 +0800

    Code Clean up

--- src/component/organisms/ProfilePage/ProfilePageDomain.js
+++ src/component/organisms/ProfilePage/ProfilePageDomain.js
@@ -1,10 +1,7 @@
 import { collection, getDoc, doc } from "firebase/firestore";
-import UserProfileService from "../../../api/services/UserProfileService";
 import { db } from "../../../firebase-config";
 import moment from 'moment';
 import { toDateTime } from '../../../utils/date';
-
-// export const currentUserProfileID = "pg9wefMNwiB1S9u8IGfT";
 
 export const getUserProfile = async (userprofileID = "") => {
     try {
@@ -12,16 +9,6 @@ export const getUserProfile = async (userprofileID = "") => {
       const userProfileData = await getDoc(userProfileRef);
 
       return userProfileData.data();
-    } catch (error) {
-      throw Error(error);
-    }
-};
-
-export const updateUserProfile = async (userprofileID = "") => {
-    try {
-      const userProfileRef = collection(db, "users")
-      const respone = await UserProfileService.getUserProfileByI;
-      return respone;
     } catch (error) {
       throw Error(error);
     }
